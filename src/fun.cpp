0ee6547c75604d7ec7a135c8a59727e18e2757ad
Rastuharem <95254106+Rastuharem@users.noreply.github.com>
2022-03-13 15:20:20 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,13 +2,65 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    int count = 0;
+    int i = 0;
+    bool isInWord = false;
+    bool hadNumber = false;
+    while (str[i] != '\0') {
+        if (str[i] == ' ') {
+            if (!hadNumber && isInWord) {
+                count++;
+            }
+            else {
+                hadNumber = false;
+            }
+            isInWord = false;
+        }
+        else {
+            isInWord == true;
+            if (hadNumber == false && isdigit(str[i])) {
+                hadNumber = true;
+            }
+        }
+        i++;
+    }
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    int count = 0;
+    int i = 0;
+    bool isInWord = false;
+    bool isGood = false;
+    bool isFirstLetter = true;
+    while (str[i] != '\0') {
+        if (str[i] == ' ') {
+            if (isGood && isInWord) {
+                count++;
+            }
+            isGood = false;
+            isInWord = false;
+            isFirstLetter = true;
+        }
+        else {
+            isInWord = true;
+            if (isFirstLetter) {
+                if (isalpha(str[i])) {
+                    isGood = true;
+                }
+                isFirstLetter = false;
+            }
+            else {
+                if (isdigit(str[i]) || isalpha(str[i])) {
+                    isGood = false;
+                }
+            }
+        }
+        i++;
+    }
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    
 }
