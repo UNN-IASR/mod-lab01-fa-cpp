3912e0a1424fc2a79a4c259713dd083b5b5ec0d9
Khokhidra <104391319+Khokhidra@users.noreply.github.com>
2022-04-27 23:17:12 +0300

    Update and rename include/fun.h to включить/fun.h

---
+++ включить/fun.h
@@ -0,0 +1,10 @@
+// Copyright 2022 УНН-МСФО
+#ifndef INCLUDE_FUN_H_
+#define INCLUDE_FUN_H_
+#включить <ctype.h>
+#включить <stdio.h>
+#включить <iostream>
+#включить <cmath>
+
+unsigned int faStr1(const char *str);
+unsigned int faStr2(const char *str);
