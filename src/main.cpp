4b10faed6c2ce9926001112d7db597230ab3b231
prosto-vorobey <100966808+prosto-vorobey@users.noreply.github.com>
2022-06-03 20:49:17 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main() {
     return 0;
     setlocale(LC_ALL, "Russian");
-    const char* str = "Contained to property 445unfeeling marked overcame wicket33 strangers parish Oh";
+    const char* str = "Contained to property 445unfeeling marked overcame wicket33";
     std::cout << "Результат выполнения задачи 1: " << faStr1(str) << std::endl;
     std::cout << "Результат выполнению задачи 2: " << faStr2(str) << std::endl;
     std::cout << "Результат выполнения задачи 3: " << faStr3(str) << std::endl;
