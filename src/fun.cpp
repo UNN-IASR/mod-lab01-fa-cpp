c4b72cf2616c0b8deeb143bfe38ee44f3963684e
katione <147106388+katione@users.noreply.github.com>
2024-03-07 16:11:55 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -6,22 +6,22 @@ unsigned int faStr1(const char *str) {
 int count = 0;
 int inWord = 0;//мы вне слова
 int number = 0;//цифра не обнаружена
-while (str[i] != '\0'){
-    if ( str[i] != ' ' && inWord == 0){
-        if ('0' <= str[i] && str[i] <= '9'){
+while (str[i] != '\0') {
+    if (str[i] != ' ' && inWord == 0) {
+        if ('0' <= str[i] && str[i] <= '9') {
             number == 1;
         }
         inWord = 1;
     }
 
-     if (str[i] != ' ' && inWord == 1){
-        if ('0' <= str[i] && str[i] <= '9'){
+     if (str[i] != ' ' && inWord == 1) {
+        if ('0' <= str[i] && str[i] <= '9') {
             number = 1;
         }
     }
 
     if (str[i] == ' ' && inWord == 1) {
-        if (number == 0){
+        if (number == 0) {
             count++;
         }
         inWord = 0;
@@ -29,7 +29,7 @@ while (str[i] != '\0'){
     }
     i++;
 }
-if (inWord == 1 && number == 0){
+if (inWord == 1 && number == 0) {
     count++;
 }
 return count;
@@ -41,22 +41,22 @@ int count = 0;
 int inWord = 0;
 int pervaiabukva = 0;// первая буква - не заглавная латинская
 int malbuk = 0;
-while (str[i] != '\0'){
-    if (str[i] != ' ' && inWord == 0){
-        if (65 <= str[i] && str[i] <= 90){
+while (str[i] != '\0') {
+    if (str[i] != ' ' && inWord == 0) {
+        if (65 <= str[i] && str[i] <= 90) {
             pervaiabukva = 1;
         }
         inWord = 1;
     }
-    if (str[i] != ' ' && inWord == 1){
-        if (97 <= str[i] && str[i] <= 122){
+    if (str[i] != ' ' && inWord == 1) {
+        if (97 <= str[i] && str[i] <= 122) {
             malbuk = 1;
-        }else{
+        } else {
             malbuk = 0;
         }
     }
-    if (str[i] == ' ' && inWord == 1){
-        if (pervaiabukva == 1 && malbuk == 1){
+    if (str[i] == ' ' && inWord == 1) {
+        if (pervaiabukva == 1 && malbuk == 1) {
             count++;
         }
         int inWord = 0;
@@ -65,7 +65,7 @@ while (str[i] != '\0'){
     }
     i++;
 }
-if (inWord == 1 && pervaiabukva == 1 && malbuk == 1){
+if (inWord == 1 && pervaiabukva == 1 && malbuk == 1) {
     count++;
 }
 return count;
@@ -76,24 +76,24 @@ unsigned int faStr3(const char *str) {
 int inWord = 0;
 int countb = 0;
 int counts = 0;
-while (str[i] != '\0'){
-     if (str[i] != ' '){
+while (str[i] != '\0') {
+     if (str[i] != ' ') {
          countb++;
          inWord = 1;
      }
-     if (str[i] == ' ' ){
-         if (inWord == 1){
+     if (str[i] == ' ') {
+         if (inWord == 1) {
              counts++;
          }
          inWord = 0;
      }
      i++;
 }
-if (inWord == 1){
+if (inWord == 1) {
     counts++;
- }
- float ans = static_cast<float>(countb) / counts;
- if ((ans - static_cast<int>(ans)) >= 0.5)
-     return static_cast<int>(ans) + 1;
- return static_cast<int>(ans);
+}
+float ans = static_cast<float>(countb) / counts;
+if ((ans - static_cast<int>(ans)) >= 0.5)
+    return static_cast<int>(ans) + 1;
+return static_cast<int>(ans);
 }
