26a1b09b63d6e40bf590b28c46e9c4c16f223ca8
Krylov Vladimir <68692106+krylofficiall@users.noreply.github.com>
2024-03-09 20:20:09 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,14 @@
 #include "fun.h"
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main() {
     const char* str;
     str = "qwe4  gftt e 555gr rrgr  grhh ";
-    cout << faStr1(str) << endl;
+    std::cout << faStr1(str) << endl;
     str = "Hello, world!";
-    cout << faStr2(str) << endl;
+    std::cout << faStr2(str) << endl;
     str = " six monkeys";
-    cout << faStr3(str) << endl;
+    std::cout << faStr3(str) << endl;
     return 0;
 }
