4c4a62ff2e40f872d11dc4e635776de46df31a98
Andrey Galanin <galaninan8@gmail.com>
2024-03-09 17:00:10 +0300

    Галанин Андрей Александрович(Писм)

--- src/fun.cpp
+++ src/fun.cpp
@@ -7,16 +7,15 @@ unsigned int faStr1(const char *str) {
     int i = 0;
     int count = 0;
     int obshag = 0;
-    while (str[i])
-    {
-        if (!flag && str[i] != ' ' ){
+    while (str[i]) {
+        if (!flag && str[i] != ' ' ) {
             flag    = true;
             count   = 1;
-        } else if (flag && str[i] == ' '){
+        } else if (flag && str[i] == ' ') {
             flag    = false;
             obshag += count;
         }
-        if (flag && isdigit(str[i])){
+        if (flag && isdigit(str[i])) {
             count = 0;
         }
         i++;
@@ -29,15 +28,14 @@ unsigned int faStr2(const char *str) {
     int i = 0;
     int count = 0;
     int obshag = 0;
-    while (str[i])
-    {
-        if (!flag && str[i] != ' ' && str[i] >= 65 && str[i] <= 90){
+    while (str[i]) {
+        if (!flag && str[i] != ' ' && str[i] >= 65 && str[i] <= 90) {
             flag    = true;
             count   = 1;
-        } else if (flag && str[i] == ' '){
+        } else if (flag && str[i] == ' ') {
             flag = false;
             obshag += count;
-        } else if (str[i] < 97 || str[i] > 122){
+        } else if (str[i] < 97 || str[i] > 122) {
             count = 0;
         }
         i++;
@@ -51,26 +49,25 @@ unsigned int faStr3(const char *str) {
     float count = 0;
     int sum = 0;
     double res = 0;
-    while (str[i])
-    {
-        if (!flag && str[i] != ' '){
+    while (str[i]) {
+        if (!flag && str[i] != ' ') {
             flag = true;
             count++;
-        } else if (flag && str[i] == ' '){
+        } else if (flag && str[i] == ' ') {
             flag = false;
 
         }
-        if (flag && str[i] != ' '){
+        if (flag && str[i] != ' ') {
             sum++;
         }
         i++;
     }
-    double intpart, fractpart; 
+    double intpart, fractpart;
     res = sum / count;
     fractpart = modf(res, &intpart);
-    if (fractpart >= 0.5){
+    if (fractpart >= 0.5) {
         return (int)res + 1;
-    } else{
+    } else {
         return (int)res;
     }
 }
