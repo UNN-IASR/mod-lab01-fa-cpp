5b89496de73f48829bfa4e3f7013cb06d10992d2
Arseniy <33318310+StatsenkoArs@users.noreply.github.com>
2024-03-09 16:33:25 +0300

    Added main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-int main() {
-    return 0;
+int main()
+{
+    char str1[] = "I have no n4mbers in 4 w0rds";
+    char str2[] = "I Have 2 words sTarTing with BIG Le11ers";
+    char str3[] = "Middle length of these words is four";
+
+    std::cout << faStr1(str1) << std::endl;
+    std::cout << faStr2(str2) << std::endl;
+    std::cout << faStr3(str3);
 }
