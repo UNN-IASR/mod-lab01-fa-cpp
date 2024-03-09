3b0de0bb9ae61b72427443d7d708c299b2917258
Krylov Vladimir <68692106+krylofficiall@users.noreply.github.com>
2024-03-09 19:54:49 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
+    const char* str;
+    str = "qwe4  gftt e 555gr rrgr  grhh ";
+    cout << faStr1(str) << endl;
+    str = "Hello, world!";
+    cout << faStr2(str) << endl;
+    str = " six monkeys";
+    cout << faStr3(str) << endl;
     return 0;
 }
