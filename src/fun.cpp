b9779e7c586c1aaf5d7d322e7e1a48d707313d05
kateandrianova <147188563+kateandrianova@users.noreply.github.com>
2024-03-09 20:59:02 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,24 +2,20 @@
 #include "fun.h"
 #include <math.h>
 
-unsigned int faStr1(const char* str)
-{
+unsigned int faStr1(const char* str){
     bool inWord = false;//изначально вне слова
     bool outNums = false;
     int count = 0;
     int i = 0;
-    while (str[i] != '\0')
-    {
+    while (str[i] != '\0'){
         if (str[i] != ' ' && inWord == false)
-        {
             inWord = true;
-        }
-        else if (!((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')) && inWord == true && str[i] != ' ')
-        {
-            outNums = true;
-        }
-        else if (str[i] == ' ' && inWord == true)
-        {
+        else 
+            if (!((str[i] >= 'A' && str[i] <= 'Z') 
+                || (str[i] >= 'a' && str[i] <= 'z'))
+                && inWord == true && str[i] != ' ')
+                outNums = true;
+        else if (str[i] == ' ' && inWord == true){
             if (outNums == false)
                 count++;
             else
@@ -30,25 +26,19 @@ unsigned int faStr1(const char* str)
     }
     return count;
 }
-    
-unsigned int faStr2(const char* str)
-{
+unsigned int faStr2(const char* str){
     bool inWord = false;//изначально вне слова
     bool outNums = false;
     int count = 0;
     int i = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] != ' ' && inWord == false && (str[i] >= 'A' && str[i] <= 'Z'))
-        {
+    while (str[i] != '\0'){
+        if (str[i] != ' ' && inWord == false 
+            && (str[i] >= 'A' && str[i] <= 'Z'))
             inWord = true;
-        }
-        else if (!(str[i] >= 'a' && str[i] <= 'z') && inWord == true && str[i] != ' ')
-        {
+        else if (!(str[i] >= 'a' && str[i] <= 'z') 
+            && inWord == true && str[i] != ' ')
             outNums = true;
-        }
-        else if (str[i] == ' ' && inWord == true)
-        {
+        else if (str[i] == ' ' && inWord == true){
             if (outNums == false)
                 count++;
             else
@@ -59,23 +49,16 @@ unsigned int faStr2(const char* str)
     }
     return count;
 }
-
-unsigned int faStr3(const char* str)
-{
+unsigned int faStr3(const char* str){
     bool inWord = false;
     int count = 0;
     int space = 0;
     int i = 0;
     int res = 0;
-    while (str[i] != '\0')
-    {
+    while (str[i] != '\0'){
         if (str[i] != ' ' && inWord == false)
-        {
             inWord = true;
-        }
-        
-        else if (str[i] == ' ' && inWord == true)
-        {
+        else if (str[i] == ' ' && inWord == true){
             inWord = false;
             count++;
         }
@@ -85,7 +68,6 @@ unsigned int faStr3(const char* str)
     }
     if (i != 0 && str[i - 1] != ' ')
         count++;
-    
     if (count != 0)
         res = round((i - space) / count);
     return res;
