8dd50af12af023085d65a96dacce0107b13e2bd5
Shalnov.Yuri.Pipr2.3Kurs <96010611+jra1251@users.noreply.github.com>
2024-03-10 02:09:01 +0300

    Revert "Update fun.cpp"

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,70 +1,14 @@
-unsigned int faStr2(const char* str) {
-	if (str == "")
-	{
-		return 0;
-	}
-	bool startworld = false;
-	int count_worlds = 0;
-	for (const char* p = str; *p; p++)
-	{
-		if (*p != ' ')
-		{
-			if (*p >= 'A' && *p <= 'Z')
-			{
-				if (!startworld)
-				{
-					startworld = true;
-				}
-				else {
-					startworld = false;
-					while (*p != ' ' && *p != 0)
-					{
-						p++;
-					}
-				}
-			}
-			else
-			{
-				if (*p >= 'a' && *p <= 'z') {
-					if (!startworld)
-					{
-						while (*p != ' ' && *p != 0)
-						{
-							p++;
-						}
-					}
+// Copyright 2022 UNN-IASR
+#include "fun.h"
 
-				}
-				else {
-					startworld = false;
-				}
+unsigned int faStr1(const char *str) {
+    return 0;
+}
 
-			}
+unsigned int faStr2(const char *str) {
+    return 0;
+}
 
-			if ('0' <= *p && *p <= '9')
-			{
-				startworld = false;
-				while (*p != ' ' && *p != 0)
-				{
-					p++;
-				}
-			}
-
-		}
-		else
-		{
-			if (startworld)
-			{
-				startworld = false;
-				count_worlds++;
-			}
-		}
-
-	}
-	if (startworld)
-	{
-		startworld = false;
-		count_worlds++;
-	}
-	return count_worlds;
+unsigned int faStr3(const char *str) {
+    return 0;
 }
