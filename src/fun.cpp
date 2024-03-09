89ebfd3cbad3ba2e353a9f29c6d59f567057e671
Andrey Galanin <galaninan8@gmail.com>
2024-03-09 16:52:58 +0300

    Галанин Андрей Александрович(Писм)

--- src/fun.cpp
+++ src/fun.cpp
@@ -3,25 +3,20 @@
 #include <ctype.h>
 #include <cmath>
 unsigned int faStr1(const char *str) {
-    bool flag   = false;
-    int i       = 0;
-    int count   = 0;
-    int obshag  = 0;
+    bool flag = false;
+    int i = 0;
+    int count = 0;
+    int obshag = 0;
     while (str[i])
     {
-        if (!flag && str[i] != ' ' )
-        {
+        if (!flag && str[i] != ' ' ){
             flag    = true;
             count   = 1;
-        }
-        else if (flag && str[i] == ' ')
-        {
+        } else if (flag && str[i] == ' '){
             flag    = false;
             obshag += count;
         }
-
-        if (flag && isdigit(str[i]))
-        {
+        if (flag && isdigit(str[i])){
             count = 0;
         }
         i++;
@@ -30,24 +25,19 @@ unsigned int faStr1(const char *str) {
 }
 
 unsigned int faStr2(const char *str) {
-    bool flag   = false;
-    int i       = 0;
-    int count   = 0;
-    int obshag  = 0;
+    bool flag = false;
+    int i = 0;
+    int count = 0;
+    int obshag = 0;
     while (str[i])
     {
-        if (!flag && str[i] != ' ' && str[i] >= 65 && str[i] <= 90)
-        {
+        if (!flag && str[i] != ' ' && str[i] >= 65 && str[i] <= 90){
             flag    = true;
             count   = 1;
-        }
-        else if (flag && str[i] == ' ')
-        {
+        } else if (flag && str[i] == ' '){
             flag = false;
             obshag += count;
-        }
-        else if (str[i] < 97 || str[i] > 122)
-        {
+        } else if (str[i] < 97 || str[i] > 122){
             count = 0;
         }
         i++;
@@ -56,25 +46,21 @@ unsigned int faStr2(const char *str) {
 }
 
 unsigned int faStr3(const char *str) {
-     bool flag   = false;
-    int i       = 0;
+     bool flag = false;
+    int i = 0;
     float count = 0;
-    int sum     = 0;
-    double res  = 0;
+    int sum = 0;
+    double res = 0;
     while (str[i])
     {
-        if (!flag && str[i] != ' ')
-        {
+        if (!flag && str[i] != ' '){
             flag = true;
             count++;
-        }
-        else if (flag && str[i] == ' ')
-        {
+        } else if (flag && str[i] == ' '){
             flag = false;
 
         }
-        if (flag && str[i] != ' ')
-        {
+        if (flag && str[i] != ' '){
             sum++;
         }
         i++;
@@ -82,12 +68,9 @@ unsigned int faStr3(const char *str) {
     double intpart, fractpart; 
     res = sum / count;
     fractpart = modf(res, &intpart);
-    if (fractpart >= 0.5)
-    {
+    if (fractpart >= 0.5){
         return (int)res + 1;
-    }
-    else
-    {
+    } else{
         return (int)res;
     }
 }
