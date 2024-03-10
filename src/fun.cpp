ce88c586c1271ea9f33e90f9b739e494de4f78fe
bartyomi <148716654+bartyomi@users.noreply.github.com>
2024-03-10 22:52:06 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,75 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <math.h>
+#include <cstring>
+#include <cctype>
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    int length = strlen(str);
+    bool isValid = false;
+    int counter = 0;
+    for (int i = 0; i < length; i++) {
+        if (isdigit(str[i])) isValid = false;
+        else if (str[i] = ' ') {
+            if (isValid) {
+                isValid = false;
+                counter++;
+            }
+            else isValid = false;
+        }
+    }
+    if (isValid) counter++;
+    return counter;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    int length = strlen(str);
+    bool isValid = false;
+    bool inWord = false;
+    int counter = 0;
+    for (int i = 0; i < length-1; i++) {
+        if (str[i] = ' ') {
+            if (isValid) {
+                isValid = false;
+                inWord = false;
+                counter++;
+            }
+            else {
+                isValid = false;
+                inWord = false;
+            }
+        }
+        else if (!inWord) {
+            inWord = true;
+            if (isupper(current)) {
+                isValid = true;
+            }
+        }
+        else if (inWord && (isupper(current) || !isalpha(current))) {
+            isValid = false;
+        }
+    }
+    if (isValid) counter++;
+    return counter;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    bool inWord = false;
+    int length = strlen(str);
+    int wordCounter = 0;
+    int letterCounter = 0;
+    for (int i = 0; i < length; i++) {
+        if (str[i] = ' ') {
+            if (inWord) {
+                inWord = false;
+                wordCounter++;
+            }
+        }
+        else {
+            inWord = true;
+            letterCounter++;
+        }
+    }
+    if (inWord) wordCounter++;
+    return round(letterCounter / wordCounter);
 }
