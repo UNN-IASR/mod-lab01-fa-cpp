bd0998530c46ff74709b521ca9df8584f2b7abd5
Mikhail <106872530+Mikhas3009@users.noreply.github.com>
2024-03-10 17:15:58 +0300

    Complete faStr1

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,8 +1,24 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
+#include <regex>
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    std::regex words("\\b[a-zA-Z]+\\b");
+    std::regex numbers("[0-9]"); 
+    std::string inputStr(str);
+    std::sregex_iterator it(inputStr.begin(), inputStr.end(), words);
+    std::sregex_iterator itEnd;
+    unsigned int count = 0;
+    while (it != itEnd) {
+        std::smatch match = *it;
+        std::string word = match.str();
+        if (std::regex_search(word, numbers) == false) {
+            count++;
+        }
+        ++it;
+    }
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
