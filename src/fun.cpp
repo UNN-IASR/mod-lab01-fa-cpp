af69914f20ccecfe3c83e6784444944663d7d5d5
Alena Poliakova <a.polyackowa2016@gmail.com>
2024-03-10 19:43:51 +0300

    draft of lab 1

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,113 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <cctype>
+#include <cmath>
 
+/**
+ * Count the number of words in the given string that do not contain digits.
+ *
+ * @param str The input string to analyze.
+ * @return The number of words without digits in the string.
+ */
 unsigned int faStr1(const char *str) {
-    return 0;
+    unsigned int count = 0; // Count of words without digits
+    bool inWord = false; // Flag to indicate if we are inside a word
+
+    while (*str) {
+        if (std::isalpha(*str)) {
+            if (!inWord) {
+                inWord = true;
+                ++count;
+            }
+        }
+        else {
+            inWord = false;
+        }
+        ++str;
+    }
+
+    return count;
 }
 
+/**
+ * Count the number of words in the given string that start with an uppercase Latin letter
+ * and consist only of lowercase Latin letters among other characters.
+ *
+ * @param str The input string to analyze.
+ * @return The number of words meeting the specified criteria.
+ */
 unsigned int faStr2(const char *str) {
-    return 0;
+    unsigned int count = 0; // Count of words meeting the criteria
+    bool inWord = false; // If we are inside a word
+    bool isLatinUppercase = false; // If the first character of the word is an uppercase Latin letter
+
+    while (*str) {
+        if (std::isalpha(*str)) {
+            if (!inWord) {
+                inWord = true;
+                if (std::isupper(*str)) {
+                    isLatinUppercase = true;
+                }
+                else {
+                    isLatinUppercase = false;
+                }
+            }
+            else {
+                if (!std::islower(*str)) {
+                    isLatinUppercase = false;
+                }
+            }
+        }
+        else {
+            if (inWord && isLatinUppercase) {
+                ++count;
+            }
+            inWord = false;
+        }
+        ++str;
+    }
+
+    if (inWord && isLatinUppercase) {
+        ++count;
+    }
+    return count;
 }
 
+/**
+ * Calculate the average length of words in the given string, rounding it to the nearest integer.
+ *
+ * @param str The input string to analyze.
+ * @return The average length of words in the string, rounded to the nearest integer.
+ */
 unsigned int faStr3(const char *str) {
-    return 0;
+    unsigned int wordCount = 0; // Count of words
+    unsigned int totalLength = 0; // Total length of words
+    bool inWord = false; // If we are inside a word
+
+    while (*str) {
+        if (std::isalpha(*str)) {
+            if (!inWord) {
+                inWord = true;
+                ++wordCount;
+            }
+            ++totalLength;
+        }
+        else {
+            inWord = false;
+        }
+        ++str;
+    }
+
+    // Calculate the average length of words
+    unsigned int averageLength = 0;
+    if (wordCount != 0) {
+        averageLength = totalLength / wordCount;
+    }
+
+    // Round the average length to the nearest integer
+    if (totalLength % wordCount * 2 >= wordCount) {
+        averageLength += 1;
+    }
+
+    return averageLength;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,16 @@
 #include "fun.h"
 
 int main() {
+    const char* str = "Hello 123 World 456";
+    unsigned int count = faStr1(str);
+    std::cout << "Number of words without digits: " << count << std::endl;
+
+    const char* str = "Hello World hEllo apple Orange";
+    unsigned int count = faStr2(str);
+    std::cout << "Number of words starting with an uppercase Latin letter and consisting only of lowercase Latin letters: " << count << std::endl;
+
+    const char* str = "This is a test string for average word length calculation";
+    unsigned int averageLength = faStr3(str);
+    std::cout << "Average word length: " << averageLength << std::endl;
     return 0;
 }
