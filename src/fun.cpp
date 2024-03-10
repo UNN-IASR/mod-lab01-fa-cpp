422ac6a4f32fd23cd115c3a027d8858ce18ec0f6
Мария <146832013+MariaBuznikova@users.noreply.github.com>
2024-03-10 21:00:56 +0300

    Бузникова Мария 3821ПИМЭ

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,106 @@
-// Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <ctype.h>
+#include <iostream>
 
-unsigned int faStr1(const char *str) {
-    return 0;
+unsigned int faStr1(const char *str)
+{
+	bool inWord = false;
+	int count = 0;
+	int i = 0;
+	bool digit = false;
+	while (str[i]) 
+	{
+		if (isdigit(str[i]) && digit == false)
+		{
+			digit = true;
+		}
+		if (str[i] != ' ' && inWord == false)
+		{
+			inWord = true;
+			count++;
+		}
+		else if (str[i] == ' ' && inWord == true)
+		{
+			if (digit == true)
+			{
+				count--;
+				digit = false;
+				
+			}
+			inWord = false;
+		}
+		i++;
+	}
+	
+	std::cout << count<<std::endl;
+	return count;
+
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr2(const char* str) {
+	bool inWord = false; 
+	int count = 0;
+	int i = 0;
+	bool Up = false;
+
+	while (str[i]) 
+	{
+		if (str[i] != ' ' && inWord == false && isupper(str[i]))
+		{
+			inWord = true;
+			count++;
+		}
+		
+		else if (str[i] != ' ' && inWord == true && !islower(str[i]) && Up == false)
+		{
+			Up = true;
+		}
+		else if (str[i] == ' ' && inWord == true)
+		{
+			inWord = false;
+			Up = false;
+		}
+		 if(Up==true)
+		 {
+			 
+			 Up = false;
+		 }
+
+
+		i++;
+	}
+	std::cout << count << std::endl;
+	return count;
+	
+    
 }
 
-unsigned int faStr3(const char *str) {
+unsigned int faStr3(const char* str) {
+	bool inWord = false; // мы еще не вошли в слово(находимся на левой границе м.Тью)
+	int count = 0;
+	int str_len = 0;
+	//int len = 0;
+	int i = 0;
+
+
+	while (str[i]) // обращение к текущему символу полное выраж - str[i]!='\0'
+	{
+		if (str[i] != ' ' && inWord == false)
+		{
+			inWord = true;
+			count++;
+		}
+		if (str[i] != ' ' && inWord == true)
+		{
+			str_len++;
+		}
+
+		else if (str[i] == ' ' && inWord == true)
+			inWord = false;
+
+		i++;
+	}
+	double average = round((double)str_len / count);
+	std::cout << average << std::endl;
     return 0;
 }
