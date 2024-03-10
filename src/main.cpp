c3abe19cd202885003d988b1163710782cfeb737
Mazniker Anton <92749509+ImperialSpaceExplorer@users.noreply.github.com>
2024-03-10 21:10:12 +0300

    Update main.cpp Мазникер

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 // Copyright 2022 UNN-IASR
+
 #include "fun.h"
 
 int main() {
-    return 0;
+	const char* str = "a1 2b c3d e F G111 Hij oPRs tuW abcd qweer t yA1 dd1b";
+	std::cout << faStr1(str) << std::endl;
+	std::cout << faStr2(str) << std::endl;
+	std::cout << faStr3(str) << std::endl;
 }
