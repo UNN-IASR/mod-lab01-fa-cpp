77d1cce184aab7d3601f60eb21a6ebe04bd63cf4
Danil Volkov <volkov20032301@yandex.ru>
2024-03-10 18:54:00 +0300

    Update format file

--- src/fun.cpp
+++ src/fun.cpp
@@ -6,81 +6,81 @@
 #include "fun.h"
 
 unsigned int faStr1(const char* str) {
-	bool inWord = false;
-	bool isDigit = false;
-	unsigned int count = 0;
-	unsigned int i = 0;
+    bool inWord = false;
+    bool isDigit = false;
+    unsigned int count = 0;
+    unsigned int i = 0;
 
-	while (str[i]) {
-		if (str[i] != ' ' && !inWord) {
-			isDigit = false;
-			inWord = true;
-			count++;
-		}
-		if (str[i] != ' ' && inWord) {
-			if (isdigit(str[i]) && !isDigit) {
-				isDigit = true;
-				count--;
-			}
-		}
-		if (str[i] == ' ' && inWord) inWord = false;
-		i++;
-	}
-	return count;
+    while (str[i]) {
+        if (str[i] != ' ' && !inWord) {
+            isDigit = false;
+            inWord = true;
+            count++;
+        }
+        if (str[i] != ' ' && inWord) {
+            if (isdigit(str[i]) && !isDigit) {
+                isDigit = true;
+                count--;
+            }
+        }
+        if (str[i] == ' ' && inWord) inWord = false;
+        i++;
+    }
+    return count;
 }
 
 unsigned int faStr2(const char* str) {
-	bool inWord = false;
-	bool isDigit = false;
-	bool isStartUpper = false;
-	bool isStartWord = false;
-	unsigned int count = 0;
-	unsigned int i = 0;
+    bool inWord = false;
+    bool isDigit = false;
+    bool isStartUpper = false;
+    bool isStartWord = false;
+    unsigned int count = 0;
+    unsigned int i = 0;
 
-	while (str[i]) {
-		if (str[i] != ' ' && !inWord) {
-			isDigit = false;
-			inWord = true;
-			isStartUpper = isupper(str[i]);
-			isStartWord = true;
-			count++;
-		}
-		if (str[i] != ' ' && inWord) {
-			// if there is no capital letter in the word
-			// - equate it to a number
-			// if there is a capital letter in the other letters
-			// - equate it to a
-			// number
-			if (!isStartUpper ||
-				((isdigit(str[i]) && !isDigit) ||
-					(!isStartWord && isupper(str[i]) && !isDigit) ||
-					(!isalpha(str[i]) && !isDigit))) {
-				isDigit = true;
-				isStartUpper = true;
-				count--;
-			}
-			isStartWord = false;
-		}
-		if (str[i] == ' ' && inWord) inWord = false;
-		i++;
-	}
-	return count;
+    while (str[i]) {
+        if (str[i] != ' ' && !inWord) {
+            isDigit = false;
+            inWord = true;
+            isStartUpper = isupper(str[i]);
+            isStartWord = true;
+            count++;
+        }
+        if (str[i] != ' ' && inWord) {
+            // if there is no capital letter in the word
+            // - equate it to a number
+            // if there is a capital letter in the other letters
+            // - equate it to a
+            // number
+            if (!isStartUpper ||
+                ((isdigit(str[i]) && !isDigit) ||
+                    (!isStartWord && isupper(str[i]) && !isDigit) ||
+                    (!isalpha(str[i]) && !isDigit))) {
+                isDigit = true;
+                isStartUpper = true;
+                count--;
+            }
+            isStartWord = false;
+        }
+        if (str[i] == ' ' && inWord) inWord = false;
+        i++;
+    }
+    return count;
 }
 
 unsigned int faStr3(const char* str) {
-	bool inWord = false;
-	unsigned int wordLength = 0;
-	unsigned int count = 0;
-	int i = 0;
+    bool inWord = false;
+    unsigned int wordLength = 0;
+    unsigned int count = 0;
+    int i = 0;
 
-	while (str[i]) {
-		if (str[i] != ' ' && inWord == false) {
-			inWord = true;
-			count++;
-		}
-		if (str[i] != ' ' && inWord) wordLength++;
-		else if (str[i] == ' ' && inWord == true) inWord = false;
-		i++;
-	}
-	return static_cast<int>(round(wordLength / count));
+    while (str[i]) {
+        if (str[i] != ' ' && inWord == false) {
+            inWord = true;
+            count++;
+        }
+        if (str[i] != ' ' && inWord) wordLength++;
+        else if (str[i] == ' ' && inWord == true) inWord = false;
+        i++;
+    }
+    return static_cast<int>(round(wordLength / count));
 }
