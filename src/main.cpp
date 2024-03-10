efe97447e440cd138b8b65ca28af32ed1930ef1f
LOMASTER12309 <yurinazarov@rambler.ru>
2024-03-10 21:22:24 +0300

    изменение main.cpp из VS 2019

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     const char* example1 = "Thg#rg 5fd ff4yh 5 .f h  ";
-    const char* example2 = " Thgrg 5fd ff4yh T5 f* H";
+    const char* example2 = " Thgrg 5fd ff4yh T5 f* H K";
     const char* example3 = " &#g^ gddg #^HDF5Hs rujrtj 46375";
     std::cout << faStr1(example1) << std::endl;
     std::cout << faStr2(example2) << std::endl;
