73408ba01d5e4eae38d58b6d155980af5f6fd3c7
YurasKamaz <69901570+YurasKamaz@users.noreply.github.com>
2024-03-10 11:32:03 +0300

    cpplint fixes

--- src/fun.cpp
+++ src/fun.cpp
@@ -6,16 +6,16 @@ unsigned int faStr1(const char *str) {
     bool isNumber = false;
     bool inWord = false;
     int counter = 0;
-    int i=0;
-    while(str[i] != '\0') {
-        if(str[i] != ' ' && !inWord) {
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && !inWord) {
             inWord = true;
             isNumber = false;
             counter++;
-        } else if((str[i] >= '0') && (str[i] <= '9') && inWord && !isNumber) {
-            isNumber = true; 
+        } else if ((str[i] >= '0') && (str[i] <= '9') && inWord && !isNumber) {
+            isNumber = true;
             counter--;
-        } else if(str[i] == ' ' && inWord) {
+        } else if (str[i] == ' ' && inWord) {
             isNumber = false;
             inWord = false;
         }
@@ -28,16 +28,16 @@ unsigned int faStr2(const char *str) {
     bool isCorrectWord = false;
     bool inWord = false;
     int counter = 0;
-    int i=0;
-    while(str[i] != '\0') {
-        if(str[i] != ' ' && !inWord) {
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && !inWord) {
             inWord = true;
             isCorrectWord = isupper(str[i]);
             counter += isCorrectWord;
-        } else if(str[i] == ' ' && inWord) {
+        } else if (str[i] == ' ' && inWord) {
             isCorrectWord = false;
             inWord = false;
-        } else if(inWord && isCorrectWord && !(str[i] >= 97 && str[i] <= 122)) {
+        } else if (inWord && isCorrectWord && !(str[i] >= 97 && str[i] <= 122)) {
             isCorrectWord = false;
             counter--;
         }
@@ -49,14 +49,14 @@ unsigned int faStr2(const char *str) {
 unsigned int faStr3(const char *str) {
     bool inWord = false;
     int counter = 0;
-    int i=0;
+    int i = 0;
     int len = 0;
-    while(str[i] != '\0') {
-        if(str[i] != ' ') {
-            if(!inWord) counter++;
+    while (str[i] != '\0') {
+        if (str[i] != ' ') {
+            if (!inWord) counter++;
             inWord = true;
             len++;
-        } else if(str[i] == ' ' && inWord) {
+        } else if (str[i] == ' ' && inWord) {
             inWord = false;
         }
         i++;
