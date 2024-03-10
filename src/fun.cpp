3ef8a2e5d9782d7577d1f8f09f4ab312d92f62dc
Анненко Миша 3821Б1ПИпр1 <146870367+AnnenkoMisha3821B1PIpr1@users.noreply.github.com>
2024-03-10 16:21:14 +0300

    Анненко Михаил 3821Б1ПИмэ b61

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,83 @@
 // Copyright 2022 UNN-IASR
+#include <ctype.h>
+#include <math.h>
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
     return 0;
+unsigned int faStr1(const char* str) {
+    int i = 0;
+    bool inWord = false;
+    bool Have_Numbers = false;
+    int Count_Words = 0;
+    while (str[i] != '\0') {
+        if (isdigit(str[i]) && Have_Numbers == false) {
+            Have_Numbers = true;
+        }
+        if (str[i] != ' ' && inWord == false) {
+            inWord = true;
+            Count_Words++;
+        } else if (str[i] == ' ' && inWord == true) {
+            if (Have_Numbers == true) {
+                Have_Numbers = false;
+                Count_Words--;
+            }
+            inWord = false;
+        }
+        i++;
+    }
+    if (inWord == true && Have_Numbers == true) {
+        Count_Words--;
+        Have_Numbers = false;
+    }
+    return Count_Words;
 }
 
 unsigned int faStr2(const char *str) {
     return 0;
+unsigned int faStr2(const char* str) {
+    int Count_Words = 0;
+    bool inW = false;
+    int i = 0;
+    bool NoLow = false;
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && inW == false && isupper(str[i])) {
+            inW = true;
+            Count_Words++;
+        } else if (str[i] != ' ' && inW == true && !islower(str[i]) && NoLow == false) {
+            NoLow = true;
+        } else if (str[i] == ' ' && inW == true) {
+            if (NoLow == true) {
+                Count_Words--;
+                NoLow = false;
+            }
+            inW = false;
+        }
+        i++;
+    }
+    return Count_Words;
 }
 
 unsigned int faStr3(const char *str) {
     return 0;
+unsigned int faStr3(const char* str) {
+    bool inWord = false;
+    double Count_Length = 0;
+    double Count_Words = 0;
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && inWord == false) {
+            inWord = true;
+            Count_Words++;
+            Count_Length++;
+        } else if (str[i] ==' ' && inWord == true) {
+            inWord = false;
+        } else if (str[i] != ' ' && inWord == true) {
+            Count_Length++;
+            inWord = true;
+        }
+        i++;
+    }
+    int Average_Value = round(Count_Length / Count_Words);
+    return Average_Value;
 }
