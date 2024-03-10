7ae1ecc4d57d55f8d2631c2a4b22746cb5f7e9af
Anna Goncharova <147106427+Goanni@users.noreply.github.com>
2024-03-10 19:43:29 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -6,28 +6,22 @@ unsigned int faStr1(const char *str) {
     int count_word = 0;
     int in_word = 0;
     int number = 0;
-    while (str[i])
-    {
-        if (str[i] != ' ' && in_word == 0)
-        {
-            if (48 <= str[i] && str[i] <= 57)
-            {
+    while (str[i]) {
+        if (str[i] != ' ' && in_word == 0) {
+            if (48 <= str[i] && str[i] <= 57) {
                 number == 1;
             }
             in_word = 1;
         }
 
-        if (str[i] != ' ' && in_word == 1)
-        {
-            if (48 <= str[i] && str[i] <= 57)
-            {
+        if (str[i] != ' ' && in_word == 1) {
+            if (48 <= str[i] && str[i] <= 57) {
                 number = 1;
             }
         }
 
         if (str[i] == ' ' && in_word == 1) {
-            if (number == 0)
-            {
+            if (number == 0) {
                 count_word++;
             }
             in_word = 0;
@@ -35,8 +29,7 @@ unsigned int faStr1(const char *str) {
         }
         i++;
     }
-    if (in_word == 1 && number == 0)
-    {
+    if (in_word == 1 && number == 0) {
         count_word++;
     }
 
@@ -46,35 +39,25 @@ unsigned int faStr1(const char *str) {
 unsigned int faStr2(const char *str) {
     int i = 0;
     int count_word = 0;
-    int in_word = 0;//мы вне слова
-    int first_letter = 0;// первая буква - не заглавная латинская 
+    int in_word = 0;
+    int first_letter = 0;
     int small_letter = 0;
-    while (str[i])
-    {
-        if (str[i] != ' ' && in_word == 0)
-        {
-            if (65 <= str[i] && str[i] <= 90)
-            {
+    while (str[i]) {
+        if (str[i] != ' ' && in_word == 0) {
+            if (65 <= str[i] && str[i] <= 90) {
                 first_letter = 1;
             }
             in_word = 1;
         }
-        if (str[i] != ' ' && in_word == 1)
-        {
-            if (97 <= str[i] && str[i] <= 122)
-            {
+        if (str[i] != ' ' && in_word == 1) {
+            if (97 <= str[i] && str[i] <= 122) {
                 small_letter = 1;
-            }
-            else
-            {
+            } else {
                 small_letter = 0;
             }
-
         }
-        if (str[i] == ' ' && in_word == 1)
-        {
-            if (first_letter == 1 && small_letter == 1)
-            {
+        if (str[i] == ' ' && in_word == 1) {
+            if (first_letter == 1 && small_letter == 1) {
                 count_word++;
             }
             int in_word = 0;
@@ -83,8 +66,7 @@ unsigned int faStr2(const char *str) {
         }
         i++;
     }
-    if (in_word == 1 && first_letter == 1 && small_letter == 1)
-    {
+    if (in_word == 1 && first_letter == 1 && small_letter == 1) {
         count_word++;
     }
     return count_word;
@@ -95,25 +77,20 @@ unsigned int faStr3(const char *str) {
     int in_word = 0;
     int count_letter = 0;
     int count_word = 0;
-    while (str[i])
-    {
-        if (str[i] != ' ')
-        {
+    while (str[i]) {
+        if (str[i] != ' ') {
             count_letter++;
             in_word = 1;
         }
-        if (str[i] == ' ')
-        {
-            if (in_word == 1)
-            {
+        if (str[i] == ' ') {
+            if (in_word == 1) {
                 count_word++;
             }
             in_word = 0;
         }
         i++;
     }
-    if (in_word == 1)
-    {
+    if (in_word == 1) {
         count_word++;
     }
     float ans = static_cast<float>(count_letter) / count_word;
