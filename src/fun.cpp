0f2a9162d3e7340903c2983d6c629823fa344222
Ekaterina <katya_ryzhevskaya@mail.ru>
2024-03-10 15:06:06 +0300

    Рыжевская Екатерина Андреевна ПИпр

--- src/fun.cpp
+++ src/fun.cpp
@@ -4,36 +4,27 @@
 #include <math.h>
 
 unsigned int faStr1(const char* str) {
-
     bool inWord = false;
     bool thisNumber = false;
     int count = 0;
     int i = 0;
-
-    while (str[i] != '\0')
-    {
-        if (str[i] != ' ' && inWord == false)
-        {
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && inWord == false) {
             inWord = true;
             thisNumber = isdigit(str[i]);
         }
-        else if (str[i] != ' ' && inWord == true)
-        {
-            if (thisNumber == false) thisNumber = isdigit(str[i]);
-        }
-        else if (str[i] == ' ' && inWord == true)
-        {
+        else if (str[i] != ' ' && inWord == true) {
             if (thisNumber == false)
-            {
+                thisNumber = isdigit(str[i]);
+        }
+        else if (str[i] == ' ' && inWord == true) {
+            if (thisNumber == false)
                 count++;
-            }
             inWord = false;
         }
-
         i++;
     }
     return count;
-
 }
 
 unsigned int faStr2(const char* str) {
@@ -41,27 +32,19 @@ unsigned int faStr2(const char* str) {
     bool isright = true;
     int count = 0;
     int i = 0;
-
-    while (str[i] != '\0')
-    {
-        if (str[i] != ' ' && inWord == false)
-        {
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && inWord == false) {
             inWord = true;
             isright = isupper(str[i]);
         }
-        else if (str[i] != ' ' && inWord == true)
-        {
+        else if (str[i] != ' ' && inWord == true) {
             if (isright == true) isright = islower(str[i]);
         }
-        else if (str[i] == ' ' && inWord == true)
-        {
+        else if (str[i] == ' ' && inWord == true) {
             if (isright == true)
-            {
                 count++;
-            }
             inWord = false;
         }
-
         i++;
     }
     return count;
@@ -72,24 +55,18 @@ unsigned int faStr3(const char* str) {
     int countWord = 0;
     int countLength = 0;
     int i = 0;
-
-    while (str[i] != '\0')
-    {
-        if (str[i] != ' ' && inWord == false)
-        {
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && inWord == false) {
             inWord = true;
             countLength++;
             countWord++;
         }
-        else if (str[i] != ' ' && inWord == true)
-        {
+        else if (str[i] != ' ' && inWord == true) {
             countLength++;
         }
-        else if (str[i] == ' ' && inWord == true)
-        {
+        else if (str[i] == ' ' && inWord == true) {
             inWord = false;
         }
-
         i++;
     }
     double length = static_cast<double>(countLength);
