bc9990bcb7cb251354967600a0a46e5605da7963
Alena Poliakova <a.polyackowa2016@gmail.com>
2024-03-10 20:10:15 +0300

    fixed naming

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,16 @@
 #include "fun.h"
 
 int main() {
-    const char* str = "Hello 123 World 456";
-    unsigned int count = faStr1(str);
+    const char* str1 = "Hello 123 World 456";
+    unsigned int count = faStr1(str1);
     std::cout << "Result of faStr1: " << count << std::endl;
 
-    const char* str = "Hello World hEllo apple Orange";
-    unsigned int count = faStr2(str);
+    const char* str2 = "Hello World hEllo apple Orange";
+    unsigned int count = faStr2(str2);
     std::cout << "Result of faStr2: " << count << std::endl;
 
-    const char* str = "This is a test string";
-    unsigned int averageLength = faStr3(str);
+    const char* str3 = "This is a test string";
+    unsigned int averageLength = faStr3(str3);
     std::cout << "Result of faStr3: " << averageLength << std::endl;
     return 0;
 }
