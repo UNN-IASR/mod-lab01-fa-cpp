d45ab4d3a201a4330846819bff00ea93d2453a8d
marikozlova <162039344+marikozlova@users.noreply.github.com>
2024-03-10 23:01:39 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -4,7 +4,6 @@
 unsigned int faStr1(const char* str) {
 	bool inWord = false, num = false;
 	int count = 0, i = 0;
-
 	while (str[i] != '\0') {
 		if (str[i] != ' ' && inWord == false) {
 			inWord = true;
@@ -25,7 +24,6 @@ unsigned int faStr1(const char* str) {
 unsigned int faStr2(const char* str) {
 	bool inWord = false, AA = false, no = false;
 	int count = 0, i = 0;
-
 	while (str[i] != '\0') {
 		if (str[i] != ' ' && inWord == false) {
 			if (inWord == false && str[i] >= 'A' && str[i] <='Z') {
@@ -54,14 +52,12 @@ unsigned int faStr3(const char* str) {
 	int i = 0;
 	float count = 0, count_s = 0;
 	float lenght = 0;
-
 	while (str[i] != '\n') {
 		if (str[i] != ' ' && inWord == false) {
 			inWord = true;
 			count++;
-		} else if (str[i] == ' ' && inWord == true)
-			inWord = false;
-		if (str[i] != ' ') count_s++;
+		} else if (str[i] == ' ' && inWord == true) { inWord = false; }
+		if (str[i] != ' ') { count_s++; }
 		i++;
 	}
 	lenght = round(count_s / count);
