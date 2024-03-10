910fa360d7d99cdea9d680308f16e7ebcc5c5b01
bartyomi <148716654+bartyomi@users.noreply.github.com>
2024-03-11 00:56:10 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,83 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <math.h>
+#include <cstring>
+#include <cctype>
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    int length = strlen(str);
+    bool isValid = false;
+    bool inWord = false;
+    int counter = 0;
+    for (int i = 0; i < length; i++) {
+        if (str[i] == ' ') {
+            inWord = false;
+            isValid = false;
+            if (isValid) {
+                counter++;
+            }
+        } else {
+            if (!inWord) {
+                isValid = true;
+            }
+            if (isdigit(str[i])) {
+                isValid = false;
+            }
+            inWord = true;
+        }
+    }
+    if (isValid) counter++;
+    return counter;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    int length = strlen(str);
+    bool isValid = false;
+    bool inWord = false;
+    int counter = 0;
+    for (int i = 0; i < length-1; i++) {
+        if (str[i] == ' ') {
+            if (isValid) {
+                isValid = false;
+                inWord = false;
+                counter++;
+            } else {
+                isValid = false;
+                inWord = false;
+            }
+        } else if (!inWord) {
+            inWord = true;
+            if (isupper(str[i])) {
+                isValid = true;
+            }
+        } else if (inWord && (isupper(str[i]) || !isalpha(str[i]))) {
+            isValid = false;
+        }
+    }
+    if (isValid) {
+        counter++;
+    }
+    return counter;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    bool inWord = false;
+    int length = strlen(str);
+    int wordCounter = 0;
+    int letterCounter = 0;
+    for (int i = 0; i < length; i++) {
+        if (str[i] == ' ') {
+            if (inWord) {
+                inWord = false;
+                wordCounter++;
+            }
+        } else {
+            inWord = true;
+            letterCounter++;
+        }
+    }
+    if (inWord) {
+        wordCounter++;
+    }
+    return round(letterCounter / wordCounter);
 }
