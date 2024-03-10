fe62625cfa40135dbd7edb5322512f3a9065d5e1
Mazniker Anton <92749509+ImperialSpaceExplorer@users.noreply.github.com>
2024-03-10 21:53:13 +0300

    cpplint test correction of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "fun.h"
 
 int main() {
-	const char* str = "a1 2b c3d e F G111 Hij oPRs tuW abcd qweer t yA1 dd1b";
-	std::cout << faStr1(str) << std::endl;
-	std::cout << faStr2(str) << std::endl;
-	std::cout << faStr3(str) << std::endl;
+const char* str = "a1 2b c3d e F G111 Hij oPRs tuW abcd qweer t yA1 dd1b Ayy";
+std::cout << faStr1(str) << std::endl;
+std::cout << faStr2(str) << std::endl;
+std::cout << faStr3(str) << std::endl;
 }
