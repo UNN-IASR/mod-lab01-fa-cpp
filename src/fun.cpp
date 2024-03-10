b6d5ee4fdf5bf8435afe5348a10b151afe7ec2a7
Alena Poliakova <a.polyackowa2016@gmail.com>
2024-03-10 21:54:47 +0300

    fixed errors

--- src/fun.cpp
+++ src/fun.cpp
@@ -12,17 +12,26 @@
 unsigned int faStr1(const char *str) {
     unsigned int count = 0;  // Count of words without digits
     bool inWord = false;  // Flag to indicate if we are inside a word
+    bool lastDigit = false;
 
     while (*str) {
-        if (std::isalpha(*str)) {
+        if (std::isalpha(*str) || std::isdigit(*str)) {
             if (!inWord) {
                 inWord = true;
-                ++count;
+                while (std::isalpha(*str) || std::isdigit(*str)) {
+                    if (std::isdigit(*str)) {
+                        std::cout << "Digit: " << *str << std::endl;
+                        inWord = false;
+                        ++str;
+                    }
+                    ++str;
+                }
+                if (inWord) ++count;
             }
         } else {
             inWord = false;
+            ++str;
         }
-        ++str;
     }
 
     return count;
@@ -35,37 +44,35 @@ unsigned int faStr1(const char *str) {
  * @param str The input string to analyze.
  * @return The number of words meeting the specified criteria.
  */
-unsigned int faStr2(const char *str) {
+unsigned int faStr2(const char* str) {
     unsigned int count = 0;  // Count of words meeting the criteria
-    bool inWord = false;  // If we are inside a word
-    // If the first character of the word is an uppercase Latin letter
-    bool isLatinUppercase = false;
+    bool valid = true;  // Flag to track validity of current word
+    int start = -1;  // Index of the start of a word
 
-    while (*str) {
-        if (std::isalpha(*str)) {
-            if (!inWord) {
-                inWord = true;
-                if (std::isupper(*str)) {
-                    isLatinUppercase = true;
-                } else {
-                    isLatinUppercase = false;
+    for (int idx = 0; str[idx] != '\0'; idx++) {
+        if (str[idx] != ' ') {
+            if (start != -1) {
+                if (!('a' <= str[idx] && str[idx] <= 'z')) {
+                    valid = false;
                 }
             } else {
-                if (!std::islower(*str)) {
-                    isLatinUppercase = false;
+                if (!('A' <= str[idx] && str[idx] <= 'Z')) {
+                    valid = false;
                 }
+                start = idx;
             }
         } else {
-            if (inWord && isLatinUppercase) {
-                ++count;
+            if (valid && start != -1) {
+                count++;
             }
-            inWord = false;
+            start = -1;
+            valid = true;
         }
-        ++str;
     }
 
-    if (inWord && isLatinUppercase) {
-        ++count;
+    // Check if the last word in the string was valid and not empty
+    if (valid && start != -1) {
+        count++;
     }
     return count;
 }
