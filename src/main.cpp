68ed8d9bbed724c5db5af888e53dff0ca0a6ab8b
Юрий <92754848+LOMASTER12309@users.noreply.github.com>
2024-03-10 15:29:02 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
-#include <iostream>
 
 int main() {
     const char* example1 = "Thg#rg 5fd ff4yh 5 .f h  ";
     const char* example2 = " Thgrg 5fd ff4yh T5 f* H";
     const char* example3 = " &#g^ gddg #^HDF5Hs rujrtj 46375";
-    std::cout << faStr1(example1) << endl;
-    std::cout << faStr2(example2) << endl;
-    std::cout << faStr3(example3) << endl;
+    std::cout << faStr1(example1) << std::endl;
+    std::cout << faStr2(example2) << std::endl;
+    std::cout << faStr3(example3) << std::endl;
     return 0;
 }
