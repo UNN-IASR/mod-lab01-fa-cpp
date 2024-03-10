1823633584a021520554fed87b621f9c825d469d
Sesan <148628462+1esan@users.noreply.github.com>
2024-03-10 18:44:10 +0300

    Лабутин Егор 3821Б1ПИсм

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,26 @@
-// Copyright 2022 UNN-IASR
-#include "fun.h"
+#include <cctype>
 
-unsigned int faStr1(const char *str) {
-    return 0;
-}
 
-unsigned int faStr2(const char *str) {
-    return 0;
-}
-
-unsigned int faStr3(const char *str) {
-    return 0;
+unsigned int faStr1(const char* str)
+{
+	int i;
+	bool invWord = false; //булевая переменная, определяющая, допустимое ли слово или нет 
+	bool word = false; //булевая переменная, определяющая, находимся ли мы в слове в данный момент
+	int WnD = 0; //Количество слов, в которых не содержутся цифры
+	while (str[i]) {
+		char value = str[i];
+		if (value == ' ') {
+			invWord = false;
+			word = false;
+		}
+		else {
+			word = true;
+			if (isdigit(value)&&!invWord) {
+				invWord = true;
+				WnD++;
+			}
+		};
+		i++;
+	}
+	return WnD;
 }
