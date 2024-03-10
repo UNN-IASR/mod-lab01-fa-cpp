11283fb6a90818a9d35fdd3b8a9ec593c62916f3
matveybobrov <matveybobrov01@gmail.com>
2024-03-10 14:55:00 +0300

    Бобров Матвей 3821Б1ПИпр

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,72 +1,92 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <cctype>
 
-unsigned int faStr1(const char *str) {
-    int i = 0;
-    int wordsWithDigit = 0;
-    bool inDigitWord = false;
-    while(str[i]) {
-        char current = str[i];
-        if (isdigit(current) && !inDigitWord) {
-            inDigitWord = true;
-            wordsWithDigit++;
-        }
-        if (current == ' ') {
-            inDigitWord = false;
-        }
-        i++;
+unsigned int faStr1(const char *str)
+{
+  int i = 0;
+  int wordsWithDigit = 0;
+  bool inDigitWord = false;
+  while (str[i])
+  {
+    char current = str[i];
+    if (isdigit(current) && !inDigitWord)
+    {
+      inDigitWord = true;
+      wordsWithDigit++;
     }
-    return wordsWithDigit;
+    if (current == ' ')
+    {
+      inDigitWord = false;
+    }
+    i++;
+  }
+  return wordsWithDigit;
 }
 
-unsigned int faStr2(const char *str) {
-    int i = 0;
-    int result = 0;
-    bool inWord = false;
-    bool inValidWord = false;
-    while(str[i]) {
-        char current = str[i];
-        if (current == ' ') {
-            if (inValidWord) {
-                result++;
-            }
-            inWord = false;
-            inValidWord = false;
-        }
-        else if (!inWord) {
-            inWord = true;
-            if (isupper(current)) {
-                inValidWord = true;
-            }
-        }
-        else if (inWord && (isupper(current) || !isalpha(current))) {
-            inValidWord = false;
-        }
-        i++;
-    }
-    if (inValidWord) {
+unsigned int faStr2(const char *str)
+{
+  int i = 0;
+  int result = 0;
+  bool inWord = false;
+  bool inValidWord = false;
+  while (str[i])
+  {
+    char current = str[i];
+    if (current == ' ')
+    {
+      if (inValidWord)
+      {
         result++;
+      }
+      inWord = false;
+      inValidWord = false;
     }
-    return result;
+    else if (!inWord)
+    {
+      inWord = true;
+      if (isupper(current))
+      {
+        inValidWord = true;
+      }
+    }
+    else if (inWord && (isupper(current) || !isalpha(current)))
+    {
+      inValidWord = false;
+    }
+    i++;
+  }
+  if (inValidWord)
+  {
+    result++;
+  }
+  return result;
 }
 
-unsigned int faStr3(const char *str) {
-    int i = 0;
-    bool inWord = false;
-    int wordsCount = 0;
-    int totalChars = 0;
-    while(str[i]) {
-        char current = str[i];
-        if (current == ' ') {
-            inWord = false;
-        } else {
-            if (!inWord) {
-                inWord = true;
-                wordsCount++;
-            }
-            totalChars++;
-        }
-        i++;
+unsigned int faStr3(const char *str)
+{
+  int i = 0;
+  bool inWord = false;
+  int wordsCount = 0;
+  int totalChars = 0;
+  while (str[i])
+  {
+    char current = str[i];
+    if (current == ' ')
+    {
+      inWord = false;
     }
-    return totalChars/wordsCount;
+    else
+    {
+      if (!inWord)
+      {
+        inWord = true;
+        wordsCount++;
+      }
+      totalChars++;
+    }
+    i++;
+  }
+  return totalChars / wordsCount;
 }
+
