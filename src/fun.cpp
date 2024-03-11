d9ba31f7066831a1221c8b48e72ce229d1ee0dc9
CSB201 <161685472+CSB201@users.noreply.github.com>
2024-03-11 14:55:37 +0300

    Реализация faStr1 и faStr2

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,11 +2,92 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    string word = "";
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (isalnum(str[i])) {
+            word += str[i];
+        }
+        else if (!word.empty()) {
+            bool hasDigit = false;
+            for (char c : word) {
+                if (isdigit(c)) {
+                    hasDigit = true;
+                    break;
+                }
+            }
+            if (!hasDigit) {
+                count++;
+            }
+            word = "";
+        }
+    }
+
+
+    if (!word.empty()) {
+        bool hasDigit = false;
+        for (char c : word) {
+            if (isdigit(c)) {
+                hasDigit = true;
+                break;
+            }
+        }
+        if (!hasDigit) {
+            count++;
+        }
+    }
+
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    bool inWord = false;
+    int UpperWordMark = 0;
+
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (isupper(str[i])) {
+            inWord = true;
+            UpperWordMark = i;
+        }
+
+
+        else if (inWord && !islower(str[i]) && !isspace(str[i])) {
+            for (int j = UpperWordMark; j <= i; j++) {
+                if (!isalpha(str[j])) {
+                    inWord = false;
+                }
+            }
+            if (inWord) {
+                count++;
+            }
+
+            inWord = false;
+        }
+
+        else if (inWord && !islower(str[i]) && isspace(str[i])) {
+            for (int j = UpperWordMark; j < i; j++) {
+                if (!isalpha(str[j])) {
+                    inWord = false;
+                }
+            }
+            if (inWord) {
+                count++;
+            }
+
+            inWord = false;
+        }
+
+        else if (inWord && islower(str[i])) {
+            continue;
+        }
+
+    }
+
+
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
