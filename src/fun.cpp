d2f504c7efecc64ad89123402737c2d8220da756
ddrzxc <147187316+ddrzxc@users.noreply.github.com>
2024-03-17 17:16:56 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -4,28 +4,27 @@
 
 unsigned int faStr1(const char *str) {
    bool flag1 = false, flag2 = false;
-    int count = 0, i = 0;
-    while (str[i]) {
-        if (str[i] != ' ' && flag1 == false) {
-            flag1 = true;
-            count++;
-        } else if (isdigit(str[i]) && flag1 == true && flag2 == false) {
-            flag2 = true;
-            count--;
-        } else if (str[i] == ' ' && flag1 == true) {
-            flag1 = false;
-            flag2 = false;
-        }
-        i++;
-    }
-    return count;
+   int count = 0, i = 0;
+   while (str[i]) {
+      if (str[i] != ' ' && flag1 == false) {
+         flag1 = true;
+         count++;
+      } else if (isdigit(str[i]) && flag1 == true && flag2 == false) {
+         flag2 = true;
+         count--;
+      } else if (str[i] == ' ' && flag1 == true) {
+         flag1 = false;
+         flag2 = false;
+      }
+      i++;
+   }
+   return count;
 }
 
 unsigned int faStr2(const char *str) {
     bool flag = false;
     int i = 0, num = 0, count = 0;
-    while (str[i])
-    {
+    while (str[i]){
         if (!flag && str[i] != ' ' && str[i] >= 65 && str[i] <= 90) {
             flag = true;
             num = 1;
@@ -44,14 +43,14 @@ unsigned int faStr3(const char *str) {
     bool flag = false;
     int i = 0, sum = 0;
     float count = 0;
-    while (str[i])
-    {
+    while (str[i]){
         if (!flag && str[i] != ' ') {
             flag = true;
             count++;
         } else if (flag && str[i] == ' ') {
             flag = false;
-        }if (flag && str[i] != ' ') {
+        }
+        if (flag && str[i] != ' ') {
             sum++;
         }
         i++;
