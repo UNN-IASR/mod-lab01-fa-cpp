44dc9f9408def9a5725a34b1440c53ab02dfe1a0
ddrzxc <147187316+ddrzxc@users.noreply.github.com>
2024-03-17 17:21:47 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,30 +1,31 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 #include <cmath>
+#include <cctype>
 
 unsigned int faStr1(const char *str) {
-   bool flag1 = false, flag2 = false;
-   int count = 0, i = 0;
-   while (str[i]) {
-      if (str[i] != ' ' && flag1 == false) {
-         flag1 = true;
-         count++;
-      } else if (isdigit(str[i]) && flag1 == true && flag2 == false) {
-         flag2 = true;
-         count--;
-      } else if (str[i] == ' ' && flag1 == true) {
-         flag1 = false;
-         flag2 = false;
-      }
-      i++;
-   }
-   return count;
+    bool flag1 = false, flag2 = false;
+    int count = 0, i = 0;
+    while (str[i]) {
+        if (str[i] != ' ' && flag1 == false) {
+            flag1 = true;
+            count++;
+        } else if (isdigit(str[i]) && flag1 == true && flag2 == false) {
+            flag2 = true;
+            count--;
+        } else if (str[i] == ' ' && flag1 == true) {
+            flag1 = false;
+            flag2 = false;
+        }
+        i++;
+    }
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
     bool flag = false;
     int i = 0, num = 0, count = 0;
-    while (str[i]){
+    while (str[i]) {
         if (!flag && str[i] != ' ' && str[i] >= 65 && str[i] <= 90) {
             flag = true;
             num = 1;
@@ -43,7 +44,7 @@ unsigned int faStr3(const char *str) {
     bool flag = false;
     int i = 0, sum = 0;
     float count = 0;
-    while (str[i]){
+    while (str[i]) {
         if (!flag && str[i] != ' ') {
             flag = true;
             count++;
