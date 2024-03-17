12019acf46e260326ae4798bf7a08fa19a6bf1e7
Мария <146832013+MariaBuznikova@users.noreply.github.com>
2024-03-17 17:25:17 +0300

    Бузникова Мария 3821ПИмэ

--- src/fun.cpp
+++ src/fun.cpp
@@ -3,20 +3,20 @@
 #include <ctype.h>
 #include <cmath>
 #include <iostream>
-unsigned int faStr1(const char *str) {
+unsigned int faStr1(const char *str){
 	bool inWord = false;
 	int count = 0;
 	int i = 0;
 	bool digit = false;
 	while (str[i]) {
-		if (isdigit(str[i]) && digit == false) {
+		if (isdigit(str[i]) && digit == false){
 			digit = true;
 		}
-		if (str[i] != ' ' && inWord == false) {
+		if (str[i] != ' ' && inWord == false){
 			inWord = true;
 			count++;
-		} else if (str[i] == ' ' && inWord == true) {
-			if (digit == true) {
+		} else if (str[i] == ' ' && inWord == true){
+			if (digit == true){
 				count--;
 				digit = false;
 			}
@@ -27,22 +27,22 @@ unsigned int faStr1(const char *str) {
 	std::cout << count<<std::endl;
 	return count;
 }
-unsigned int faStr2(const char* str) {
+unsigned int faStr2(const char* str){
 	bool inWord = false; 
 	int count = 0;
 	int i = 0;
 	bool Up = false;
 	while (str[i]) {
-		if (str[i] != ' ' && inWord == false && isupper(str[i])) {
+		if (str[i] != ' ' && inWord == false && isupper(str[i])){
 			inWord = true;
 			count++;
-		} else if (str[i] != ' ' && inWord == true && !islower(str[i]) && Up == false) {
+		} else if (str[i] != ' ' && inWord == true && !islower(str[i]) && Up == false){
 			Up = true;
-		} else if (str[i] == ' ' && inWord == true) {
+		} else if (str[i] == ' ' && inWord == true){
 			inWord = false;
 			Up = false;
 		}
-		if (Up == true) {
+		if (Up == true){
 			 Up = false;
 		 }
 		i++;
@@ -50,19 +50,19 @@ unsigned int faStr2(const char* str) {
 	std::cout << count << std::endl;
 	return count;
 }
-unsigned int faStr3(const char* str) {
+unsigned int faStr3(const char* str){
 	bool inWord = false;
 	double count = 0;
 	double str_len = 0;
 	int i = 0;
 	while (str[i]) {
-		if (str[i] != ' ' && inWord == false) {
+		if (str[i] != ' ' && inWord == false){
 			inWord = true;
 			count++;
 		}
-		if (str[i] != ' ' && inWord == true) {
+		if (str[i] != ' ' && inWord == true){
 			str_len++;
-		} else if (str[i] == ' ' && inWord == true) {
+		} else if (str[i] == ' ' && inWord == true){
 			inWord = false;
 		}
 		i++;
