4bbd8bcb808090bfc5358de90487e8786d730d95
s1aveyan <147203365+s1aveyan@users.noreply.github.com>
2024-03-17 17:48:16 +0300

    update cpplint fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,88 +1,89 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
+#include <cmath>
+#include <cctype>
 
-unsigned int faStr1(const char *str) {
-        bool InWord=false;
-    bool IsNum=false;
-    int counter=0;
-    int i=0;
-    while(str[i]!='\0'){
-        if(str[i]!=' '&&InWord==false){
-            InWord=true;
-            if(isdigit(str[i])){
-                IsNum=true;
+unsigned int faStr1(const char* str) {
+    bool InWord = false;
+    bool IsNum = false;
+    int counter = 0;
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && InWord == false) {
+            InWord = true;
+            if (isdigit(str[i])) {
+                IsNum = true;
             }
             counter++;
-        }else if(str[i]!=' '&&InWord==true){
-        
-            if(isdigit(str[i])){
-                IsNum=true;
+        } else if (str[i] != ' ' && InWord == true) {
+            if (isdigit(str[i])) {
+                IsNum = true;
             }
-        }else if(str[i]==' '&&InWord==true){
-            InWord=false;
-            if(IsNum){
+        } else if (str[i] == ' ' && InWord == true) {
+            InWord = false;
+            if (IsNum) {
                 counter--;
             }
-            IsNum=false;
+            IsNum = false;
         }
         i++;
     }
     return counter;
 }
 
-unsigned int faStr2(const char *str) {
-    bool InWord=false;
-    bool Fail=false;
-    int counter=0;
-    int i=0;
-    while(str[i]!='\0'){
-        if(str[i]!=' '&&InWord==false){
-            InWord=true;
-            if(islower(str[i])||isdigit(str[i])){
-                Fail=true;
+unsigned int faStr2(const char* str) {
+    bool InWord = false;
+    bool Fail = false;
+    int counter = 0;
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && InWord == false) {
+            InWord = true;
+            if (islower(str[i]) || isdigit(str[i])) {
+                Fail = true;
             }
             counter++;
-        }else if(str[i]!=' '&&InWord==true){
-        
-            if(isupper(str[i])||isdigit(str[i])){
-                Fail=true;
+        } else if (str[i] != ' ' && InWord == true) {
+            if (isupper(str[i]) || isdigit(str[i])) {
+                Fail = true;
             }
-        }else if(str[i]==' '&&InWord==true){
-            InWord=false;
-            if(Fail){
+        } else if (str[i] == ' ' && InWord == true) {
+            InWord = false;
+            if (Fail) {
                 counter--;
             }
-            Fail=false;
+            Fail = false;
         }
         i++;
     }
-    if(InWord==true){
-        if(Fail){
-                counter--;
-            }
-            Fail=false;
+    if (InWord == true) {
+        if (Fail) {
+            counter--;
+        }
+        Fail = false;
     }
     return counter;
 }
 
-unsigned int faStr3(const char *str) {
-    bool InWord=false;
-    int counter=0;
-    int i=0;
-    int len=0;
-    while(str[i]!='\0'){
-        if(str[i]!=' '&&InWord==false){
-            InWord=true;
+unsigned int faStr3(const char* str) {
+    bool InWord = false;
+    int counter = 0;
+    int i = 0;
+    int len = 0;
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && InWord == false) {
+            InWord = true;
             len++;
-        }else if(str[i]==' '&&InWord==true){
+        } else if (str[i] == ' ' && InWord == true) {
             counter++;
-        }else if(str[i]!=' '&&InWord==true){
+        } else if (str[i] != ' ' && InWord == true) {
             len++;
         }
         i++;
     }
-    if(InWord==true){
+    if (InWord == true) {
         counter++;
     }
-    return round(static_cast<float>(len)/counter);
+    return round(static_cast<float>(len) / counter);
 }
