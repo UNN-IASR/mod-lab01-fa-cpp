0ea6a43fb2a9bd8ad3809e90a567afd11fb8d473
Мария <146832013+MariaBuznikova@users.noreply.github.com>
2024-03-17 17:38:26 +0300

    Бузникова ПИмэ

--- src/fun.cpp
+++ src/fun.cpp
@@ -3,19 +3,19 @@
 #include <ctype.h>
 #include <cmath>
 #include <iostream>
-unsigned int faStr1(const char *str){
+unsigned int faStr1(const char *str) {
 	bool inWord = false;
 	int count = 0;
 	int i = 0;
 	bool digit = false;
 	while (str[i]) {
-		if (isdigit(str[i]) && digit == false){
+		if (isdigit(str[i]) && digit == false) {
 			digit = true;
 		}
-		if (str[i] != ' ' && inWord == false){
+		if (str[i] != ' ' && inWord == false) {
 			inWord = true;
 			count++;
-		} else if (str[i] == ' ' && inWord == true){
+		} else if (str[i] == ' ' && inWord == true) {
 			if (digit == true){
 				count--;
 				digit = false;
@@ -27,47 +27,50 @@ unsigned int faStr1(const char *str){
 	std::cout << count<<std::endl;
 	return count;
 }
-unsigned int faStr2(const char* str){
+unsigned int faStr2(const char* str) {
 	bool inWord = false; 
 	int count = 0;
 	int i = 0;
-	bool Up = false;
+	bool Down = false;
 	while (str[i]) {
-		if (str[i] != ' ' && inWord == false && isupper(str[i])){
+		if (str[i] != ' ' && inWord == false && isupper(str[i])) {
 			inWord = true;
 			count++;
-		} else if (str[i] != ' ' && inWord == true && !islower(str[i]) && Up == false){
-			Up = true;
+		} else if (str[i] != ' ' && inWord == true && !islower(str[i])) {
+			if (Down == false) {
+				Down = true;
+			}
 		} else if (str[i] == ' ' && inWord == true){
+			if (Down == true){
+				count--;
+				Down = false;
+			}
 			inWord = false;
-			Up = false;
 		}
-		if (Up == true){
-			 Up = false;
-		 }
 		i++;
 	}
 	std::cout << count << std::endl;
 	return count;
 }
-unsigned int faStr3(const char* str){
+unsigned int faStr3(const char* str) {
 	bool inWord = false;
 	double count = 0;
 	double str_len = 0;
 	int i = 0;
 	while (str[i]) {
-		if (str[i] != ' ' && inWord == false){
+		if (str[i] != ' ' && inWord == false) {
 			inWord = true;
 			count++;
-		}
-		if (str[i] != ' ' && inWord == true){
 			str_len++;
-		} else if (str[i] == ' ' && inWord == true){
+		} else if (str[i] != ' ' && inWord == true) {
+			str_len++;
+			inWord = true;
+		} else if (str[i] == ' ' && inWord == true) {
 			inWord = false;
 		}
 		i++;
 	}
-	double average = round(str_len / count);
+	int average = round(str_len / count);
 	std::cout << average << std::endl;
 	return average;
 }
