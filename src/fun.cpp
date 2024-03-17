04f5419d3a258b49bb844b4a4d972784b4fe7124
Mikhail <106872530+Mikhas3009@users.noreply.github.com>
2024-03-17 16:53:41 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,68 +1,79 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 #include <iostream>
-#include <regex>
+#include "math.h"
+#include "ctype.h"
 
-unsigned int faStr1(const char *str) {
-    std::regex words("\\b[a-zA-Z]+\\b");
-    std::regex numbers("[0-9]"); 
-    std::string inputStr(str);
-    std::sregex_iterator it(inputStr.begin(), inputStr.end(), words);
-    std::sregex_iterator itEnd;
-    unsigned int count = 0;
-    while (it != itEnd) {
-        std::smatch match = *it;
-        std::string word = match.str();
-        if (std::regex_search(word, numbers) == false) {
-            count++;
-        }
-        ++it;
-    }
-    return count;
-}
-
-unsigned int faStr2(const char *str) {
-    std::regex wordRegex(R"([A-Z][a-z,.]+)");
-    std::string inputStr(str);
-    std::sregex_iterator it(inputStr.begin(), inputStr.end(), wordRegex);
-    std::sregex_iterator itEnd;
-    unsigned int count = 0;
-    for (; it != itEnd; ++it) {
-        const std::smatch& match = *it;
-        bool includesSeparators = false;
-        for (char c : match.str()) {
-            if (c == ' ' || c == ',' || c == '.') {
-                includesSeparators = true;
-                break;
-            }
-        }
-        if (!includesSeparators) {
-            ++count;
-        }
-    }
-    return count;
-}
-
-unsigned int faStr3(const char *str) {
-    std::string inputStr(str);
-    int wordCount = 0;
-    int totalWordLength = 0;
+unsigned int faStr1(const char* input) {
+    unsigned int wordCount = 0;
     bool inWord = false;
-    for (char c : inputStr) {
-        if (std::isalpha(c)) {
+    bool hasNumbers = false;
+    int i = 0;
+    while (input[i] != '\0') {
+        if (!inWord && input[i] != ' ') {
             inWord = true;
-            totalWordLength++;
-        } 
-        else if (inWord) {
-            wordCount++;
+            hasNumbers = isdigit(input[i]);
+        } else if (inWord && input[i] != ' ') {
+            if (isdigit(input[i]))
+                hasNumbers = true;
+        }
+        if (inWord && (input[i] == ' ' || input[i + 1] == '\0')) {
+            if (!hasNumbers)
+                wordCount++;
+            hasNumbers = false;
             inWord = false;
         }
+
+        i++;
     }
-    if (inWord) {
-        wordCount++;
-    }
-    if (wordCount == 0) {
-        return 0; 
-    }
-    return totalWordLength / wordCount;
+    return wordCount;
 }
+
+unsigned int faStr2(const char* input) {
+    unsigned int wordCount = 0;
+    bool inWord = false;
+    bool isGood = true;
+    int i = 0;
+    while (input[i] != '\0') {
+        if (!inWord && input[i] != ' ') {
+            inWord = true;
+            isGood = isupper(input[i]);
+        } else if (inWord && input[i] != ' ') {
+            if (!islower(input[i]))
+                isGood = false;
+        }
+        if (inWord && (input[i] == ' ' || input[i + 1] == '\0')) {
+            if (isGood)
+                wordCount++;
+            isGood = true;
+            inWord = false;
+        }
+        i++;
+    }
+    return wordCount;
+}
+
+unsigned int faStr3(const char* input) {
+    unsigned int numWords = 0;
+    unsigned int totalLength = 0;
+    bool inWord = false;
+    int i = 0;
+    while (input[i] != '\0') {
+        if (!inWord && input[i] != ' ') {
+            inWord = true;
+            numWords++;
+            totalLength++;
+        } else if (inWord && input[i] != ' ') {
+            totalLength++;
+        }
+        if (inWord && (input[i] == ' ' || input[i + 1] == '\0')) {
+            inWord = false;
+        }
+        i++;
+    }
+
+    double averageLength = static_cast<double>(totalLength / numWords);
+
+    return static_cast<int>(round(averageLength));
+}
+
