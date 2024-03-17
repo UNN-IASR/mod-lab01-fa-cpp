995b53926efc77a402bcc5079872f1a523518c1a
Shalnov.Yuri.Pipr2.3Kurs <96010611+jra1251@users.noreply.github.com>
2024-03-17 14:37:51 +0300

    Шальнов Юрий ПИсм проверка

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,30 +1,30 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 unsigned int faStr1(const char* str) {
-    if (str == ""){
-		return 0;
+    if (str == "") {
+	    return 0;
 	}
 	bool startworld = false;
 	int count_worlds = 0;
-	for (const char* p = str; *p; p++){
+	for (const char* p = str; *p; p++) {
         if (*p != ' '){
 			startworld = true;
-			if ('0' <= *p && *p <= '9'){
+			if ('0' <= *p && *p <= '9') {
 				startworld = false;
-				while (*p != ' '){
+				while (*p != ' ') {
 					p++;
 				}
 			}
 
-		}else{
-			if (startworld){
-				startworld = false;
+		} else {
+			if (startworld) {
+			    startworld = false;
 				count_worlds++;
 			}
 		}
 
 	}
-	if (startworld){
+	if (startworld) {
 		startworld = false;
 		count_worlds++;
 	}
@@ -32,29 +32,27 @@ unsigned int faStr1(const char* str) {
 }
 
 
-unsigned int faStr2(const char* str){
-	if (str == ""){
+unsigned int faStr2(const char* str) {
+	if (str == "") {
 		return 0;
 	}
 	bool startworld = false;
 	int count_worlds = 0;
-	for (const char* p = str; *p; p++){
-		if (*p != ' '){
-			if (*p >= 'A' && *p <= 'Z'){
+	for (const char* p = str; *p; p++) {
+		if (*p != ' ') {
+			if (*p >= 'A' && *p <= 'Z') {
 				if (!startworld){
 					startworld = true;
-				}
-				else {
+				} else {
 					startworld = false;
-					while (*p != ' ' && *p != 0){
+					while (*p != ' ' && *p != 0) {
 						p++;
 					}
 				}
-			}
-			else{
+			} else {
 				if (*p >= 'a' && *p <= 'z') {
 					if (!startworld){
-						while (*p != ' ' && *p != 0){
+						while (*p != ' ' && *p != 0) {
 							p++;
 						}
 					}
@@ -64,25 +62,21 @@ unsigned int faStr2(const char* str){
 					startworld = false;
 				}
 
-			}
-
-			if ('0' <= *p && *p <= '9'){
+			} if ('0' <= *p && *p <= '9') {
 				startworld = false;
-				while (*p != ' ' && *p != 0){
+				while (*p != ' ' && *p != 0) {
 					p++;
 				}
 			}
 
-		}
-		else{
-			if (startworld){
+		} else { 
+			if (startworld) {
 				startworld = false;
 				count_worlds++;
 			}
 		}
 
-	}
-	if (startworld){
+	} if (startworld) {
 		startworld = false;
 		count_worlds++;
 	}
@@ -92,32 +86,30 @@ unsigned int faStr2(const char* str){
 
 unsigned int faStr3(const char* str) {
 	int count_worlds = 0, countSymbol = 0;
-	if (str == ""){
+	if (str == "") {
 		return 0;
 	}
 	bool startworld = false;
-	for (const char* p = str; *p; p++){
-		if (*p != ' '){
+	for (const char* p = str; *p; p++) {
+		if (*p != ' ') {
 			startworld = true;
 			countSymbol++;
-			if ('0' <= *p && *p <= '9'){
+			if ('0' <= *p && *p <= '9') {
 				startworld = false;
-				while (*p != ' ' && *p != 0){
+				while (*p != ' ' && *p != 0) {
 					p++;
 				}
 			}
 
-		}
-		else{
-			if (startworld){
+		} else {
+			if (startworld) {
 				startworld = false;
 				count_worlds++;
 			}
 		}
 
-	}
-	if (startworld){
-		startworld = false;
+	} if (startworld) {
+	    startworld = false;
 		count_worlds++;
 	}
 	int mediumLength = countSymbol / count_worlds;
