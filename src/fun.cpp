4c2f733462cdff495cd3ed46869de9dd0286e5c9
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2024-03-18 19:13:53 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,13 +2,16 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    
+    return 1;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    
+    return 1;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    
+    return 1;
 }
