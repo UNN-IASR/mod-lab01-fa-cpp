66833c8545d341346942f720e3dd438161ee5e7b
napoleon1716 <119057003+napoleon1716@users.noreply.github.com>
2024-03-25 16:55:59 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
 
 int main() {
+    const char* str = " six monkeys";
+    
+    std::cout << "Number of words without digits: " << faStr1(str) << std::endl;
+    std::cout << "Number of words starting with uppercase letter: " << faStr2(str) << std::endl;
+    std::cout << "Average word length: " << faStr3(str) << std::endl;
+    
     return 0;
 }
