e26431a5695037e16b95de4b0e6b4472d0d0628b
napoleon1716 <119057003+napoleon1716@users.noreply.github.com>
2024-03-25 17:09:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,9 @@
 #include "fun.h"
 
 int main() {
+    std::cout << "Number of words without digits: " << faStr1(str) << std::endl;
+    std::cout << "Number of words starting with uppercase letter: " << faStr2(str) << std::endl;
+    std::cout << "Average word length: " << faStr3(str) << std::endl;
+    
     return 0;
 }
